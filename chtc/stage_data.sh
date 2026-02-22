b6dd07ea0f118c6b5ce0d8fe3c1795c679c47772
lexu27 <leoxu27@gmail.com>
2026-02-22 12:12:28 -0600

    forgot to stage dta

--- chtc/stage_data.sh
+++ chtc/stage_data.sh
@@ -9,7 +9,9 @@ REPO_ID="${3:-local/collab}"
 TRANSFER_HOST="transfer.chtc.wisc.edu"
 GROUP_STAGING_DIR="/staging/groups/hagenow_group"
 DATASET_DIR="${LEROBOT_HOME}/${REPO_ID}"
-TARBALL="/tmp/collab_dataset.tar.gz"
+DATASET_NAME="${REPO_ID##*/}"
+STAGED_TARBALL_NAME="${DATASET_NAME}_dataset.tar.gz"
+TARBALL="/tmp/${STAGED_TARBALL_NAME}"
 
 if [ ! -d "$DATASET_DIR" ]; then
   echo "ERROR: dataset directory not found: $DATASET_DIR"
@@ -20,8 +22,8 @@ echo "Creating tarball from: $DATASET_DIR"
 tar -czf "$TARBALL" -C "$LEROBOT_HOME" "$REPO_ID"
 echo "Tarball size: $(du -sh "$TARBALL" | cut -f1)"
 
-echo "Uploading to ${TRANSFER_HOST}:${GROUP_STAGING_DIR}/collab_dataset.tar.gz"
-scp "$TARBALL" "${NETID}@${TRANSFER_HOST}:${GROUP_STAGING_DIR}/collab_dataset.tar.gz"
+echo "Uploading to ${TRANSFER_HOST}:${GROUP_STAGING_DIR}/${STAGED_TARBALL_NAME}"
+scp "$TARBALL" "${NETID}@${TRANSFER_HOST}:${GROUP_STAGING_DIR}/${STAGED_TARBALL_NAME}"
 
 echo "Done."
 rm -f "$TARBALL"
